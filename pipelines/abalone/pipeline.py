33d75c422b5035ad56827ab43f0b4c44bd15c64d
Henrique Bittencourt <74229794+gopita@users.noreply.github.com>
2023-07-28 13:59:11 -0300

    Update pipeline.py

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -151,7 +151,6 @@ def get_pipeline(
 
     # parameters for pipeline execution
     processing_instance_count = ParameterInteger(name="ProcessingInstanceCount", default_value=1)
-    processing_instance_type = 'ml.t3.medium'
     model_approval_status = ParameterString(
         name="ModelApprovalStatus", default_value="PendingManualApproval"
     )
